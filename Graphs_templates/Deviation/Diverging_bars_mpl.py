f8d5bd05f252df74f5a042a13feb0463cee46069
Mila <114945926+miladziekanowska@users.noreply.github.com>
2023-04-24 18:46:58 +0000

    Deviation graphs added

---
+++ Graphs_templates/Deviation/Diverging_bars_mpl.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# let's load the data
+diamonds = sns.load_dataset('diamonds')
+
+# and prepare it
+df = diamonds.groupby('color').sum(numeric_only=True)
+x = df.loc[:, ['price']]
+df['s_price'] = (x - x.mean())/x.std() # here we standardize the price
+df['colors'] = ['red' if x < 0 else 'green' for x in df['s_price']] # and assign the rules we will follow
+df.sort_values('s_price', inplace=True)
+df.reset_index(inplace=True)
+
+# Draw plot
+plt.figure(dpi= 80)
+plt.hlines(y=df['color'], xmin=0, xmax=df['s_price'], color=df['colors'], alpha=0.4, linewidth=5)
+
+# Decorations
+plt.gca().set(ylabel='$Color$', xlabel='$Price$')
+plt.yticks(df['color'], fontsize=12)
+plt.title('Diverging Bars of diamonds\'s price', fontdict={'size':20})
+plt.grid(linestyle='--', alpha=0.5)
+plt.show()
+
+plt.savefig('Diverging_Bar_mpl.png')
